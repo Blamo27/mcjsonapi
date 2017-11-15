fed9575c62252d46da48c595771e0c252a117ed2
Nicholai Nissen <nicholainissen@gmail.com>
2017-11-15 18:05:59 +0100

    feat(manager): Introduce mapInstances function

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # mcjsonapi
+___
 [![CircleCI](https://img.shields.io/circleci/project/github/Nicholaiii/mcjsonapi.svg?style=for-the-badge)](https://circleci.com/gh/Nicholaiii/mcjsonapi)
-
+[![npm](https://img.shields.io/npm/v/mcjsonapi.svg?style=for-the-badge)](https://www.npmjs.com/package/mcjsonapi)
+[![npm](https://img.shields.io/npm/l/mcjsonapi.svg?style=for-the-badge)](https://github.com/Nicholaiii/mcjsonapi/blob/master/LICENSE)
+![](https://img.shields.io/badge/WINTER-IS%20COMING-blue.svg?style=for-the-badge)
 
 Control and monitor your Minecraft server with JSONAPI
 
--- src/manager.js
+++ src/manager.js
@@ -5,19 +5,44 @@ const Minecraft = require('./minecraft')
  */
 const manager = {
 	instances: {},
+	/**
+	 * Sets instance, overriding any existing ones with same name.
+	 * @param  {String} name Instance name
+	 * @param  {Object} opts Instance options
+	 * @return {Minecraft}   Minecraft instance.
+	 */
 	setInstance(name, opts) {
 		this.instances[name] = new Minecraft(opts)
 		return this.instances[name]
 	},
+	/**
+	 * Returns instance with given name
+	 * @param  {String} name Instance name
+	 * @return {Minecraft}   Minecraft instance.
+	 */
 	getInstance(name) {
 		return this.instances[name]
 	},
+	/**
+	 * Ensures instance, returns existing or creates a new instance with provided opts.
+	 * @param  {String} name Instance name
+	 * @param  {Object} opts Instance options
+	 * @return {Minecraft}   Minecraft instance.
+	 */
 	ensureInstance(name, opts) {
 		if (name in this.instances) {
 			return this.instances[name]
 		}
 		this.instances[name] = new Minecraft(opts)
 		return this.instances[name]
+	},
+	/**
+	 * Maps over instances
+	 * @param  {Function} cb Callback function. Called with params key and value.
+	 * @return {[Any]}  	Returns result of mapping
+	 */
+	mapInstances(cb) {
+			return Object.entries(this.instances).map(e => cb(...e))
 	}
 }
 
@@ -26,14 +51,39 @@ class Manager {
 		Object.assign(this, manager)
 		this.instances = {}
 	}
+	/**
+	 * Sets instance, overriding any existing ones with same name.
+	 * @param  {String} name Instance name
+	 * @param  {Object} opts Instance options
+	 * @return {Minecraft}   Minecraft instance.
+	 */
 	static setInstance(name, val) {
 		return manager.setInstance(name, val)
 	}
+	/**
+	 * Returns instance with given name
+	 * @param  {String} name Instance name
+	 * @return {Minecraft}   Minecraft instance.
+	 */
 	static getInstance(name) {
 		return manager.getInstance(name)
 	}
+	/**
+	 * Ensures instance, returns existing or creates a new instance with provided opts.
+	 * @param  {String} name Instance name
+	 * @param  {Object} opts Instance options
+	 * @return {Minecraft}   Minecraft instance.
+	 */
 	static ensureInstance(name, opts) {
 		return manager.ensureInstance(name, opts)
+	}
+	/**
+	 * Maps over instances
+	 * @param  {Function} cb Callback function. Called with params key and value.
+	 * @return {[Any]}  	Returns result of mapping
+	 */
+	static mapInstances(cb) {
+		return manager.mapInstances(cb)
 	}
 }
 
